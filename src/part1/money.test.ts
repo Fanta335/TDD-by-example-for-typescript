780ea6f8701b66e25de846ffd13c96a18901b8e8
Fanta335 <fantayusuke5210@gmail.com>
2023-01-11 14:51:15 +0900

    chapter 6

--- src/part1/money.test.ts
+++ src/part1/money.test.ts
@@ -1,20 +1,23 @@
-import { Money } from './money';
+import { Dollar } from './dollar';
+import { Franc } from './franc';
 
 describe('Money test', () => {
   it('test multiplication', () => {
-    const five = new Money.Dollar(5);
-    expect(new Money.Dollar(10)).toEqual(five.times(2));
-    expect(new Money.Dollar(15)).toEqual(five.times(3));
+    const five = new Dollar(5);
+    expect(new Dollar(10)).toEqual(five.times(2));
+    expect(new Dollar(15)).toEqual(five.times(3));
   });
 
   it('test equality', () => {
-    expect(new Money.Dollar(5).equals(new Money.Dollar(5))).toBeTruthy();
-    expect(new Money.Dollar(5).equals(new Money.Dollar(4))).toBeFalsy();
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+    expect(new Dollar(5).equals(new Dollar(4))).toBeFalsy();
+    expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
+    expect(new Franc(5).equals(new Franc(4))).toBeFalsy();
   });
 
   it('test franc multiplication', () => {
-    const five = new Money.Franc(5);
-    expect(new Money.Franc(10)).toEqual(five.times(2));
-    expect(new Money.Franc(15)).toEqual(five.times(3));
+    const five = new Franc(5);
+    expect(new Franc(10)).toEqual(five.times(2));
+    expect(new Franc(15)).toEqual(five.times(3));
   });
 });
--- src/part1/money.ts
+++ src/part1/money.ts
@@ -1,35 +1,12 @@
-export namespace Money {
-  export class Dollar {
-    private amount: number;
+export class Money {
+  protected amount: number;
 
-    constructor(amount: number) {
-      this.amount = amount;
-    }
-
-    times(multiplier: number): Dollar {
-      return new Dollar(this.amount * multiplier);
-    }
-
-    equals(object: Dollar): boolean {
-      const dollar = object;
-      return this.amount === dollar.amount;
-    }
+  constructor(amount: number) {
+    this.amount = amount;
   }
 
-  export class Franc {
-    private amount: number;
-
-    constructor(amount: number) {
-      this.amount = amount;
-    }
-
-    times(multiplier: number): Franc {
-      return new Franc(this.amount * multiplier);
-    }
-
-    equals(object: Franc): boolean {
-      const franc = object;
-      return this.amount === franc.amount;
-    }
+  equals(object: Money): boolean {
+    const money = object;
+    return this.amount === money.amount;
   }
 }
