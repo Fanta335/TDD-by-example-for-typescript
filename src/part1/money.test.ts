19e1da542015f9de0e123dd71233454351be6b96
Fanta335 <fantayusuke5210@gmail.com>
2023-01-11 13:05:26 +0900

    chapter 2

--- src/part1/money.test.ts
+++ src/part1/money.test.ts
@@ -3,7 +3,7 @@ import { Money } from './money';
 describe('Money test', () => {
   it('test multiplication', () => {
     const five = new Money.Dollar(5);
-    five.times(2);
-    expect(10).toBe(five.amount);
+    const product = five.times(2);
+    expect(10).toBe(product.amount);
   });
 });
