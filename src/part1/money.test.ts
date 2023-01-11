16fbd638de56d24b9b716d99fa78703ee309b370
Fanta335 <fantayusuke5210@gmail.com>
2023-01-11 13:35:32 +0900

    chapter 4

--- src/part1/money.test.ts
+++ src/part1/money.test.ts
@@ -3,8 +3,8 @@ import { Money } from './money';
 describe('Money test', () => {
   it('test multiplication', () => {
     const five = new Money.Dollar(5);
-    const product = five.times(2);
-    expect(10).toBe(product.amount);
+    expect(new Money.Dollar(10)).toEqual(five.times(2));
+    expect(new Money.Dollar(15)).toEqual(five.times(3));
   });
 
   it('test equality', () => {
